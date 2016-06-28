c5ba176bc190fc6c28616c73631dd64b9980edb3
Igor Stark <igor@MacBook-Pro-de-Igor.local>
2016-06-28 11:45:04 +0200

    step 2 - la List view avec bouchon

--- SoNantes/DealsListViewController.swift
+++ SoNantes/DealsListViewController.swift
@@ -8,13 +8,22 @@
 
 import UIKit
 
-class DealsListViewController: UIViewController {
+class DealsListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate { // Les 2 derniers sont des protocoles
     // Prorpiété ci-dessous créée par drag and drop (bouton droit) de la Table View ici
     @IBOutlet weak var myDealTableView: UITableView!
-
+    var myDealsList : Array<Deal> = Array<Deal>()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.myDealTableView.delegate = self // Le délégué est la classe DealsListViwController elle-même
+        self.myDealTableView.dataSource = self
+        
+        self.myDealsList = DealWebService.GET(Dictionary<String, String>())
+        
+        
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +31,22 @@ class DealsListViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // Rédéfinition ==> UITableViewDelegate
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let myCell : UITableViewCell = UITableViewCell()
+        let myDeal : Deal = myDealsList[indexPath.row]
+        
+        myCell.textLabel?.text = myDeal.dealTitle
+        myCell.detailTextLabel?.text = myDeal.dealDescription
+        return myCell
+    }
+    
+    // Redéfinition ==> dans DataSource
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.myDealsList.count
+    }
+    
     
 
     // MARK: - Navigation
